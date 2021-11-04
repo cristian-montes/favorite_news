812b810189931b36660f3dd849531a5ba9c9ca48
Cristian Montes <cmontesmujica@gmail.com>
2021-11-04 15:38:31 -0600

    passed needed probs to newsarticle component

--- src/components/newsArticles/NewsArticleList.jsx
+++ src/components/newsArticles/NewsArticleList.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import NewsArticle from './NewsArticle';
 
 
-export default function NewsArticleList(){
-    return  <NewsArticle/>
+export default function NewsArticleList({articles}){
+    const articleList = articles.map(({title, author, description,urlToImage, url}) => (
+        <li key={`${title}-${author}`}> 
+            <NewsArticle
+                title={title}
+                author= {author}
+                description={description}
+                image={urlToImage}
+                articleURL={url}
+            />
+        </li>
+    ));
+
+
+    return (
+        <ul aria-label="articles">{articleList}</ul>
+   )
 }
